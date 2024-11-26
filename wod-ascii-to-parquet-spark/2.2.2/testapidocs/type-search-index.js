c625fb88ca66b93c8333e10e477e4bf7d8f12da0
Jenkins <ci-cmg-jenkins@example.com>
2024-11-26 01:17:25 +0000

    Update Documentation: wod-ascii-to-parquet-spark/2.2.2

---
+++ wod-ascii-to-parquet-spark/2.2.2/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.colorado.cires.wod.spark.w2p","l":"SparklerExecutorLocalTest"},{"p":"edu.colorado.cires.wod.spark.w2p","l":"SparklerExecutorTest"}]
